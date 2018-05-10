953d68575f483a6a88083de09ef5b1c10a7595ff
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-10 17:42:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,34 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each.with_index(1) do |dwarf, index|
-    puts "#{index}. #{dwarf}"
-  end
-end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# <<<<<<< HEAD
+# def roll_call_dwarves(dwarves)
+#   dwarves.each.with_index(1) do |dwarf, index|
+#     puts "#{index}. #{dwarf}"
+#   end
+# end
+#
+# # def roll_call_dwarves(roll_call)
+# #   i = 0
+# #   while i < roll_call.length
+# #     puts "#{i + 1}. #{roll_call[i]}"
+# #     i += 1
+# #   end
+# # end
+#
+# =======
 # def roll_call_dwarves(roll_call)
 #   i = 0
 #   while i < roll_call.length
@@ -11,22 +36,33 @@ end
 #     i += 1
 #   end
 # end
-
-def summon_captain_planet(summoning_words)
-  summoning_words.map do |word|
-    "#{word.capitalize}" + "!"
-  end
-end
-
-# assorted_words = ["two", "go", "industrious", "bop"]
-def long_planeteer_calls(assorted_words)
-  assorted_words.any? { |word| word.length > 4 }
-end
-
-def find_the_cheese(food)
-  cheese_types = %w(cheddar gouda comembert)
-
-  food.find do |maybe_cheese|
-    cheese_types.include?(maybe_cheese)
-  end
-end
+#
+# >>>>>>> bb1d861323b13c6381e65c06b884fb7be6335766
+# def summon_captain_planet(summoning_words)
+#   summoning_words.map do |word|
+#     "#{word.capitalize}" + "!"
+#   end
+# end
+#
+# # assorted_words = ["two", "go", "industrious", "bop"]
+# def long_planeteer_calls(assorted_words)
+# <<<<<<< HEAD
+#   assorted_words.any? { |word| word.length > 4 }
+# =======
+#   i = 0
+#   while i < assorted_words.length
+#     if assorted_words[i].length > 4
+#       true
+#     i += 1
+#     end
+#   end
+# >>>>>>> bb1d861323b13c6381e65c06b884fb7be6335766
+# end
+#
+# def find_the_cheese(food)
+#   cheese_types = %w(cheddar gouda comembert)
+#
+#   food.find do |maybe_cheese|
+#     cheese_types.include?(maybe_cheese)
+#   end
+# end
